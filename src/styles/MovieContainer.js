4eca4a714255f99c10c52ea2f8f4a300b69c314b
pachecosamuel <pacheco.samuel@hotmail.com>
2024-06-18 11:18:04 -0300

    funcional

---
+++ src/styles/MovieContainer.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MoviesContainer = styled.section`
+    padding: 10px;
+    background: #cecece;
+    border: 5px solid black;
+    border-radius: 10px;
+`
