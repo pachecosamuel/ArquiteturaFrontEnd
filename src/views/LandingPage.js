05ce36751b5794efb7562f353ae11ef6261cca0c
pachecosamuel <pachecosamuel@hotmail.com>
2024-07-24 13:22:17 -0300

    retirando import inutilizado

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Layout } from "antd";
-import { SimpleForm } from "../components/form/SimpleForm"
 import { TopSection } from "../components/containers/TopSection"
 import { AboutSection } from "../components/containers/AboutSection"
 import { FooterSection } from "../components/containers/FooterSection"
